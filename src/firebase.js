2500550e9c37f039f97e794f73cbace48edff4b7
mihir2129-bit <mihir2129@gmail.com>
2026-07-02 11:30:12 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,16 @@
-// 1. Go to https://console.firebase.google.com -> Create a project (free)
-// 2. In the project, click "Build" -> "Realtime Database" -> Create database -> start in TEST mode
-// 3. Click the gear icon -> Project settings -> scroll to "Your apps" -> click the </> (web) icon
-// 4. Register the app, copy the firebaseConfig object it gives you, and paste it below.
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDDkIF2OL5llHhjpd4R-nBwqizlA5ls2ck",
+  authDomain: "brainwar-93757.firebaseapp.com",
+  databaseURL: "https://brainwar-93757-default-rtdb.firebaseio.com",
+  projectId: "brainwar-93757",
+  storageBucket: "brainwar-93757.firebasestorage.app",
+  messagingSenderId: "1096785440559",
+  appId: "1:1096785440559:web:ec9db8fe23599470fc7bda",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+window._fbDb = db;
